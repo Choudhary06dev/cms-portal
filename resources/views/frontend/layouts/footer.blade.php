4a32ddc12e4b47a6e67913326c115e226ff0b0de
Ahmadsaeed9490 <ahmadsaeed9490@gmail.com>
2025-10-31 11:31:15 +0500

    frontend complate

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -66,13 +66,14 @@
             <svg class="me-2 mt-1 flex-shrink-0" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
             </svg>
-             <span class="text-white">+92 300 0000000</span>
+             <span class="text-white"> +92 300 7924806</span>
           </li>
           <li class="d-flex align-items-start">
             <svg class="me-2 mt-1 flex-shrink-0" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
               <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
             </svg>
-             <span class="text-white">Lahore, Pakistan</span>
+             <span class="text-white">Lahore, Pakistan<br>
+                                                Office#915 High,Q Tower Gulberg V, jail Road.</span>
           </li>
         </ul>
       </div>
@@ -98,6 +99,9 @@
             <svg width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
               <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
             </svg>
+          </a>
+          <a href="#" class="btn btn-outline-light btn-sm rounded-circle p-2" style="width: 38px; height: 38px;">
+            <i class="bi bi-youtube" style="font-size:18px;"></i>
           </a>
         </div>
       </div>
